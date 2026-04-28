4dd7f9b06929880c4d914c60273ca3d8dd8b0b3c
Melissa Francielle <129883268+Melissa-Francielle@users.noreply.github.com>
2026-04-28 12:07:42 -0300

    Implement battle finalization after combat
    Added battle finalization logic after the combat loop.

--- main.lua
+++ main.lua
@@ -17,34 +17,35 @@ utils.printCreature(boss)
 playerActions.build()
 bossActions.build()
 
-    while true do 
-        print()
-        print(string.format("Qual será sua ação %s?", player.name))
-        local validPlayerActions = playerActions.getValidACtions(player, colossus)
-        for i, action in pairs(validPlayerActions) do
-            print(string.format("%d, %s", i, action.description))
-        end
-        local chosenIndex = utils.ask()
-        local chosenAction = validPlayerActions[chosenIndex]
-        local isActionValid = chosenAction ~= nil
-
+while true do 
+    print()
+    print(string.format("Qual será sua ação %s?", player.name))
+    local validPlayerActions = playerActions.getValidACtions(player, colossus)
+    for i, action in pairs(validPlayerActions) do
+        print(string.format("%d, %s", i, action.description))
+    end
+    local chosenIndex = utils.ask()
+    local chosenAction = validPlayerActions[chosenIndex]
+    local isActionValid = chosenAction ~= nil
         if isActionValid then
             chosenAction.execute(player, boss)
         else
             print(string.format("Sua escolha é invalida, %s perdeu o turno", player.name))
         end
 
-        if boss.health <= 0 then
-            break
-        end
-        
-        print()
-        local validBossActions = bossActions.getValidACtions(player, boss)
-        local bossActions = validBossActions[math.random(#validBossActions)]
-        bossActions.execute(player, boss)
+    print()
+    local validBossActions = bossActions.getValidACtions(player, boss)
+    local bossActions = validBossActions[math.random(#validBossActions)]
+    bossActions.execute(player, boss)
 
-        if player.health <= 0 then
-            break
-        end
-
+    if player.health <= 0 then
+        break
     end
+
+
+    if boss.health <= 0 then
+            break
+    end
+end
+
+utils.battleFinilization(player, boss)
